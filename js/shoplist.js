df42f7750cbd7d81d830423e91998b0f8bf760b6
TAHiRLI <tahrli.tahirr@gmail.com>
2022-11-06 16:17:33 +0400

    slider btns added

--- js/shoplist.js
+++ js/shoplist.js
@@ -1,14 +1,19 @@
 //=======================================================
 // SLIDER AUTO SCROLL
 //=======================================================
+//  WITH BUTTONS 
 
 
+const nextBtn = document.querySelector('.right_btn');
+const prevBtn = document.querySelector('.left_btn');
 const cardContainer = document.querySelector('.slider_cards');
 const cards = document.querySelectorAll('.slider_cards_card');
+let cardWidth = document.querySelector('.slider_cards  :first-child').offsetWidth + 24;
+
 //parameters
-const perView = 5;
+const perView = 4;
 let scrollCount = 0;
-let delay = 2000;
+let delay = 5000;
 
 cardContainer.style.setProperty('--per-view', perView);
 for (let i = 0; i < perView; i++) {
@@ -16,22 +21,63 @@ for (let i = 0; i < perView; i++) {
 }
 
 let autoscroll = setInterval(() => {
-    scroll();
+    scroll(true);
 }, delay);
 
-const scroll = () => {
-    scrollCount++;
+const scroll = (direction) => {
+    // !!! bool Direction == true => slide left 
+    if (direction)
+        scrollCount++;
+
     if (scrollCount == cards.length + 1) {
         scrollCount = 1;
-        clearInterval(autoscroll);
-        cardContainer.style.transition = '0s';
         cardContainer.style.left = 0;
-        autoscroll = setInterval(() => {
-            scroll();
-        }, delay);
+        cardContainer.style.transition = '0s';
+
+        refreshInterval();
 
     }
-    const cardWidth = document.querySelector('.slider_cards  :first-child').offsetWidth + 24;
+    if (scrollCount == -1) {
+        scrollCount = cards.length;
+        cardContainer.style.transition = '0s';
+        cardContainer.style.left = 0;
+        cardContainer.style.transition = '0s';
+        refreshInterval();
+    }
+
+    cardWidth = document.querySelector('.slider_cards  :first-child').offsetWidth + 24;
+
+    if(scrollCount != cards.length)
     cardContainer.style.transition = '0.3s';
-    cardContainer.style.left = `-${scrollCount * cardWidth}px`;
+
+
+    if (direction)
+        cardContainer.style.left = `-${scrollCount * cardWidth}px`;
+    if (!direction) {
+        cardContainer.style.left = `-${scrollCount * cardWidth}px`;
+    }
+
+
+};
+
+//====================================
+// adding btn events
+//====================================
+nextBtn.addEventListener('click', () => {
+    scroll(true);
+    refreshInterval();
+});
+
+prevBtn.addEventListener('click', () => {
+    scrollCount--;
+    scroll(false);
+    refreshInterval();
+});
+
+
+const refreshInterval = () => {
+    clearInterval(autoscroll);
+    autoscroll = setInterval(() => {
+        scroll(true);
+    }, delay);
 };
