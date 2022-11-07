879ead2468f02ff512709423b70a932bfdfa31d7
TAHiRLI <tahrli.tahirr@gmail.com>
2022-11-07 10:54:42 +0400

    minor changes

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,7 @@
 const nav = document.querySelector('.nav-scroll');
 
 window.addEventListener ('scroll',()=>{
-    console.log(document.documentElement.scrollTop)
     if(document.documentElement.scrollTop > window.innerHeight){
-        console.log('calis')
         nav.style.top = '0';
     }
     else{
