b09c0df147b9dd4e0cb5c071361a38e5e68ace69
TAHiRLI <tahrli.tahirr@gmail.com>
2022-11-11 21:50:45 +0400

    gridhome cards sets id to localstorage

--- js/gridHome.js
+++ js/gridHome.js
@@ -135,40 +135,53 @@ document.body.addEventListener('click', (e) => {
     }
 });
 
-
-
 const getData = (category) => {
-    fetch('./js/dummy.json').then(res => res.json()).then(data => {
+    fetch('./js/dummy.json').then(res => res.json())
+        .then(data => {
 
-        const filteredData = data?.filter(product => {
-            if (category === 'ALL')
-                return true;
-            return product.category.includes(category);
-        });
+            const filteredData = data?.filter(product => {
+                if (category === 'ALL')
+                    return true;
+                return product.category.includes(category);
+            });
 
 
 
-        container.innerHTML = ''
-        filteredData?.map(product => {
+            container.innerHTML = '';
+            filteredData?.map(product => {
 
-            let status = 'New';
-            if (product.count <= 0)
-                status = "Sold";
+                let status = 'New';
+                if (product.count <= 0)
+                    status = "Sold";
 
-            container.innerHTML += `  <div id="${product.id}" class="cards_card">
+                container.innerHTML += `  <div id="${product.id}" class="cards_card">
         <div class="cards_card_status ${status}">${status}</div>
-        <a  class="cards_card_info">
+        <a href ="standartProduct.html" target="_blank" class="cards_card_info">
           <p class="flower-name">${product.name}</p>
           <p class="flower-price">${product.price}</p>
           <button class="addCart-btn">Add to cart</button>
         </a>
         <img src="${product.url}" width="100%" alt="" />
         </div>`;
+            });
+
+        })
+
+        .then(() => {
+            const cardlinks = document.getElementsByClassName('cards_card_info');
+            console.log(cardlinks);
+            for (const link of cardlinks) {
+                link.addEventListener('click',()=>{
+                    let productId =link.parentElement.getAttribute('id');
+                    localStorage.setItem('wantedItem',productId);
+                })
+            }
         });
+};
 
-    });
-}
+getData('ALL');
 
 
 
-getData('ALL')
+
+
